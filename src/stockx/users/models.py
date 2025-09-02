5b943c214070d20bd387480beabea18cf88a2c80
origin-unknown <ich.hier.wer.dort@gmx.de>
2025-09-02 23:16:39 +0200

    first commit

--- src/stockx/users/models.py
+++ src/stockx/users/models.py
@@ -4,6 +4,7 @@ from flask_bcrypt import (
 	check_password_hash
 )
 from flask_login import UserMixin
+from hashlib import md5, sha256 
 from typing import List
 
 
@@ -30,3 +31,6 @@ class User(db.Model, UserMixin):
 	def verify_password(self, password):
 		return check_password_hash(self.password_hash, password)
 
+	def avatar(self, size):
+		digest = sha256(self.email.lower().encode('utf-8')).hexdigest()
+		return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
