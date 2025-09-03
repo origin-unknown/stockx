1eadd216a8556ca76fca197c0141e262bfd018e8
origin-unknown <ich.hier.wer.dort@gmx.de>
2025-09-03 23:42:39 +0200

    first commit

--- src/stockx/trading/forms.py
+++ src/stockx/trading/forms.py
@@ -4,11 +4,19 @@ from flask_login import current_user
 from flask_wtf import FlaskForm
 from sqlalchemy import func
 from wtforms import DecimalField, IntegerField, SelectField, StringField, SubmitField
-from wtforms.validators import DataRequired, NumberRange, ValidationError
+from wtforms.validators import DataRequired, NumberRange, Regexp, ValidationError
 
 
 class BuyForm(FlaskForm):
-	symbol = StringField('Symbol', validators=[DataRequired()])
+	symbol = StringField('Symbol', 
+		validators=[
+			DataRequired(), 
+			Regexp(
+				r'^[A-Z0-9]{1,5}(?:[.:][A-Z]{1,3})?$', 
+				message='Invalid symbol'
+			)
+		]
+	)
 	# Allow fractional numbers
 	# shares = DecimalField('Number of Shares', places=3, validators=[NumberRange(min=0)])
 	shares = IntegerField('Number of Shares', validators=[DataRequired(), NumberRange(min=1)])
@@ -16,7 +24,15 @@ class BuyForm(FlaskForm):
 	submit = SubmitField('Buy')
 
 class SellForm(FlaskForm):
-	symbol = StringField('Symbol', validators=[DataRequired()])
+	symbol = StringField('Symbol', 
+		validators=[
+			DataRequired(), 
+			Regexp(
+				r'^[A-Z0-9]{1,5}(?:[.:][A-Z]{1,3})?$', 
+				message='Invalid symbol'
+			)
+		]
+	)
 	# Allow fractional numbers
 	# shares = DecimalField('Number of Shares', places=3, validators=[NumberRange(min=0)])
 	shares = IntegerField('Number of Shares', validators=[DataRequired(), NumberRange(min=1)])
